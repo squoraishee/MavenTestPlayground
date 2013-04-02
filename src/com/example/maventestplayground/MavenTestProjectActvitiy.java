eb29d5362395c9faaf69900146aac50b2c726659
squoraishee <Shafik.Quoraishee@viacommix.com>
2013-04-02 14:57:44 -0400

    first commit

---
+++ src/com/example/maventestplayground/MavenTestProjectActvitiy.java
@@ -0,0 +1,18 @@
+package com.example.maventestplayground;
+
+
+import com.example.mavetestplayround.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+
+public class MavenTestProjectActvitiy extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maven_test_project);
+    }
+
+}
