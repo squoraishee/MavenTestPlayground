7d7a224f1af50a8120aa685d604b1fbf31484c43
Shafik.Quoraishee <Shafik.Quoraishee@viacommix.com>
2013-06-28 08:59:32 -0400

    updating build script

---
+++ android_builder.sh
@@ -0,0 +1,1 @@
+mvn -Dandroid.sdk.path=/Users/hudson/android -Dbuild.number=${BUILD_VERSION} -Dsign.keystore="/Users/android/hudson/cert/mtvn.cert" -Dsign.alias="mtvn" -Dsign.storepass="mtvn1234Cert" -Dkeypass="mtvn1234Alias" -Dbuild.name=$JOB_NAME -Dandroid-maven-plugin.version=3.6.0 clean install
