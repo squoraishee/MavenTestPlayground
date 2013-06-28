441b131eb675c37480e3503967667b059a04cdda
Shafik.Quoraishee <Shafik.Quoraishee@viacommix.com>
2013-06-28 13:40:57 -0400

    Move all

--- android_builder.sh
+++ android_builder.sh
@@ -1,2 +1,2 @@
 echo "Running Android Builder..."
-mvn -Dandroid.sdk.path=/Users/hudson/android -Dbuild.number=${BUILD_VERSION} -Dsign.keystore="/Users/hudson/android/cert/mtvn.cert" -Dsign.alias="mtvn" -Dsign.storepass="mtvn1234Cert" -Dkeypass="mtvn1234Alias" -Dbuild.name=$JOB_NAME -Dandroid-maven-plugin.version=3.6.0 clean install
+mvn -Dandroid.sdk.path=/Users/hudson/android -Dbuild.number=${BUILD_VERSION} -Dsign.keystore="/Users/hudson/android/cert/mtvn.cert" -Dsign.alias="mtvn" -Dsign.password="mtvn1234Cert" -Dkeypass="mtvn1234Alias" -Dbuild.name=$JOB_NAME -Dandroid-maven-plugin.version=3.6.0 clean install
